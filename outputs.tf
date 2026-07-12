e0d7d52b870e6fecb6d437c69f3bd54626338cea
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "backup_protected_file_shares" {
-  description = "All backup_protected_file_share resources"
-  value       = azurerm_backup_protected_file_share.backup_protected_file_shares
-}
 output "backup_protected_file_shares_backup_policy_id" {
-  description = "List of backup_policy_id values across all backup_protected_file_shares"
-  value       = [for k, v in azurerm_backup_protected_file_share.backup_protected_file_shares : v.backup_policy_id]
+  description = "Map of backup_policy_id values across all backup_protected_file_shares, keyed the same as var.backup_protected_file_shares"
+  value       = { for k, v in azurerm_backup_protected_file_share.backup_protected_file_shares : k => v.backup_policy_id }
 }
 output "backup_protected_file_shares_recovery_vault_name" {
-  description = "List of recovery_vault_name values across all backup_protected_file_shares"
-  value       = [for k, v in azurerm_backup_protected_file_share.backup_protected_file_shares : v.recovery_vault_name]
+  description = "Map of recovery_vault_name values across all backup_protected_file_shares, keyed the same as var.backup_protected_file_shares"
+  value       = { for k, v in azurerm_backup_protected_file_share.backup_protected_file_shares : k => v.recovery_vault_name }
 }
 output "backup_protected_file_shares_resource_group_name" {
-  description = "List of resource_group_name values across all backup_protected_file_shares"
-  value       = [for k, v in azurerm_backup_protected_file_share.backup_protected_file_shares : v.resource_group_name]
+  description = "Map of resource_group_name values across all backup_protected_file_shares, keyed the same as var.backup_protected_file_shares"
+  value       = { for k, v in azurerm_backup_protected_file_share.backup_protected_file_shares : k => v.resource_group_name }
 }
 output "backup_protected_file_shares_source_file_share_name" {
-  description = "List of source_file_share_name values across all backup_protected_file_shares"
-  value       = [for k, v in azurerm_backup_protected_file_share.backup_protected_file_shares : v.source_file_share_name]
+  description = "Map of source_file_share_name values across all backup_protected_file_shares, keyed the same as var.backup_protected_file_shares"
+  value       = { for k, v in azurerm_backup_protected_file_share.backup_protected_file_shares : k => v.source_file_share_name }
 }
 output "backup_protected_file_shares_source_storage_account_id" {
-  description = "List of source_storage_account_id values across all backup_protected_file_shares"
-  value       = [for k, v in azurerm_backup_protected_file_share.backup_protected_file_shares : v.source_storage_account_id]
+  description = "Map of source_storage_account_id values across all backup_protected_file_shares, keyed the same as var.backup_protected_file_shares"
+  value       = { for k, v in azurerm_backup_protected_file_share.backup_protected_file_shares : k => v.source_storage_account_id }
 }
 
